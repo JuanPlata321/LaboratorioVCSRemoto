08ab557913a3f7f068fc624e9ffdddfe45f8b68b
JuanPlata321 <juan.plata.2018@upb.edu.co>
2020-03-26 22:20:27 -0500

    Mi primer commit

--- main.py
+++ main.py
@@ -18,3 +18,20 @@ d = float(input("Ingrese el número d: "))
 
 print("El cuadrado de b es: "+ str(b**2))
 print("La raiz cuadrada de d es: "+ str(d**(1/2))) 
+
+#%% Punto 3
+
+a = float(input("Ingrese el número a: "))
+b = float(input("Ingrese el número b: "))
+c = float(input("Ingrese el número c: "))
+d = (-4*a*c+b**2)**0.5
+
+if ((-4*a*c+b**2))<0:
+    print("La ecuación no tiene solución")
+else:
+    if d>0:
+        print("La solucion de la ecuación cuadrática positiva es: "+ str((-b+(d))/(2*a)))
+        print("La solucion de la ecuación cuadrática negativa es: "+ str((-b-(d))/(2*a)))
+
+    if d==0:
+        print("X1 y X2 son iguales y corresponden a -b/2a: ")

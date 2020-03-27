418d9f391c787da3c90cb7e8c8c45cee208442e7
JuanPlata321 <juan.plata.2018@upb.edu.co>
2020-03-26 20:27:31 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 20:12:19 2020
+
+@author: JuanPa
+"""
+
+n1 = int(input("Ingrese el número a: "))
+n2 = int(input("Ingrese el número c: "))
+
+print("El resultado de a por c es: " + str (n1*n2))
+print("El doble del número a es: " + str (n1*2))
